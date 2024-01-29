f2fec4d7230594157e61f84e6d8eb427835181a2
natcurti <naticurti01@gmail.com>
2024-01-29 15:40:24 -0300

    Fix CSS on PostCard component

--- src/Pages/AboutMe/index.js
+++ src/Pages/AboutMe/index.js
@@ -8,6 +8,9 @@ const AboutMe = () => {
         <PostModel cover={coverPicture} title="Sobre Mim">
             <h3 className={styles.subtitle}>Olá, eu sou a Natália!</h3>
             <img src={picture} alt="Foto da Natália Curti" className={styles.pictureAboutMe}/>
+            <p className={styles.text}>Meu nome é Natalia, tenho 28 anos e atualmente estou cursando Sistemas de Informação. Estou me especializando em Desenvolvimento Front-End e procuro uma oportunidade de estágio na área.</p>
+            <p className={styles.text}>Além da faculdade, também sou estudante da Alura e da DIO. Sou apaixonada por estudar e também sou apaixonada por tecnologia e estou constantemente buscando me aprimorar e desenvolver minhas habilidades.</p>
+            <p className={styles.text}>Sou uma pessoa proativa, com forte senso de responsabilidade e comprometida com a qualidade do meu trabalho. Tenho facilidade de trabalhar em equipe e estou sempre disposta a aprender coisas novas.</p>
         </PostModel>       
     )
 }
