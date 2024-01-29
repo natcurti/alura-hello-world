71f66be9b5d2bf26a04feab07142ce221613aa9a
natcurti <naticurti01@gmail.com>
2024-01-29 11:11:56 -0300

    Apply useNavigate

--- src/Pages/NotFound/index.js
+++ src/Pages/NotFound/index.js
@@ -6,7 +6,6 @@ import { useNavigate } from 'react-router-dom';
 const NotFound = () => {
     const navigate = useNavigate();
 
-
     return (
         <>
         <div className={styles.contentContainer}>
